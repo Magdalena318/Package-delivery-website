b6609d94cc13d4b6147318e23e42573d3bc76f3f
Daria <karpovad@student.mini.pw.edu.pl>
2020-11-04 18:19:19 +0100

    Lookup displays all info on a provided package number.

--- Back-endNew/Back-endNew/Models/Database.cs
+++ Back-endNew/Back-endNew/Models/Database.cs
@@ -15,21 +15,23 @@ namespace Back_endNew.Models
             packages = new List<Package>();
             last_package_id = 10000000;
 
-            //Name pickup_name = new Name("John", "Doe");
-            //Address pickup_address = new Address("USA", "New York", "Washington st. 52", "51464", 52.6234, 54.45124);
-            //EndpointDetails pickup_details = new EndpointDetails(pickup_name, pickup_address);
+            Name pickup_name = new Name("John", "Doe");
+            Address pickup_address = new Address("USA", "New York", "Washington st. 52", "51464", 52.6234, 54.45124);
+            string pickup_date = "21/32/2025";
+            EndpointDetails pickup_details = new EndpointDetails(pickup_name, pickup_address, pickup_date);
 
-            ////Parsing delivery details
-            //Name delivery_name = new Name("Lisa", "Kekington");
-            //Address delivery_address = new Address("France", "Paris", "Rouge st. 21", "21152", 12.65453, 54.13545);
-            //EndpointDetails delivery_details = new EndpointDetails(delivery_name, delivery_address);
+            //Parsing delivery details
+            Name delivery_name = new Name("Lisa", "Kekington");
+            Address delivery_address = new Address("France", "Paris", "Rouge st. 21", "21152", 12.65453, 54.13545);
+            string delivery_date = "21/04/2005";
+            EndpointDetails delivery_details = new EndpointDetails(delivery_name, delivery_address, delivery_date);
 
-            ////Parsing package_details
-            //PackageInfo package_info = new PackageInfo("big enough", 52.3);
+            //Parsing package_details
+            PackageInfo package_info = new PackageInfo("big enough", 52.3);
 
-            ////Creating and adding a new package
-            //Package new_package = new Package(last_package_id, pickup_details, delivery_details, package_info);
-            //this.AddNewPackage(new_package);
+            //Creating and adding a new package
+            Package new_package = new Package(last_package_id, pickup_details, delivery_details, package_info);
+            this.AddNewPackage(new_package);
         }
 
         public Package FindPackage(int id)
--- Front-end/script.js
+++ Front-end/script.js
@@ -1,43 +1,100 @@
 window.onload = function load(){
 	
-	
 	window.scrollTo(0, 0);
 	
 	//Looking up a package
 	document.getElementById("lookup_button_open_form").onclick = function Open_package_number(){
-		document.getElementById("type_form").remove();
-		document.getElementById("package_number_form").style.visibility = "visible";
+		//document.getElementById("type_form").style.display = "none";
+		document.getElementById("type_form").style.display = "none";
+		document.getElementById("package_number_form").style.display = "grid";
 	}
 	
 	document.getElementById("lookup_button").onclick = function Lookup_package(){
 		 var package_number = document.getElementById("package_number").value;
+		 //var display_package_form = document.getElementById("display_package_form");
 		 if(package_number == null || package_number == ""){
 			 window.alert("Please enter the package number!");
 			 return;
 		 } else {	
 
 			//Sending GET request
-			var address = 'https://localhost:44327/api/Packages'  + package_number.toString();
+			var address = 'https://localhost:44327/api/Packages/'  + package_number.toString();
 			fetch(address, {
 				method: 'GET',
 			})
 				.then(response => response.json())
 				.then(data => {
-				  console.log('Success:', data);
+					console.log('Success:', data);
+					document.getElementById("package_number_form").style.display = "none";
+					document.getElementById("sender_details").style.display = "none";
+					document.getElementById("receiver_details").style.display = "none";
+					document.getElementById("package_details").style.display = "none";
+					
+					//Displaying pickup details
+					document.getElementById("div_pickup_fname").innerText = data.pickup_details.name.first_name;
+					document.getElementById("div_pickup_lname").innerText = data.pickup_details.name.last_name;
+					document.getElementById("div_pickup_country").innerText = data.pickup_details.address.country;
+					document.getElementById("div_pickup_city").innerText = data.pickup_details.address.city;
+					document.getElementById("div_pickup_address").innerText = data.pickup_details.address.address;
+					document.getElementById("div_pickup_index").innerText = data.pickup_details.address.index;
+					document.getElementById("div_pickup_date").innerText = data.pickup_details.date;
+					
+					var change_pickup_lat = Number(data.pickup_details.address.latlng.lat);
+					var change_pickup_lng = Number(data.pickup_details.address.latlng.lng);
+					var change_latlng = L.latLng(change_pickup_lat, change_pickup_lng);
+					console.log(change_latlng);
+					var change_pickup_map = L.map("change_pickup_location").setView(change_latlng, 13);
+					L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+						maxZoom: 18,
+						id: 'mapbox/streets-v11',
+						tileSize: 512,
+						zoomOffset: -1,
+						accessToken: 'pk.eyJ1IjoibWFnZGFsZW5hMzE4IiwiYSI6ImNrZ2ljeHp3eTAyZXIydHN1bTFmcTdxZG0ifQ.UwB5VeAMvyI5EfZ6kA4KSQ'
+					}).addTo(change_pickup_map);
+					var change_pickup_marker = L.layerGroup().addTo(change_pickup_map);
+					var marker = L.marker(change_latlng).addTo(change_pickup_marker);
+					
+					//Displaying delivery details
+					document.getElementById("div_delivery_fname").innerText = data.delivery_details.name.first_name;
+					document.getElementById("div_delivery_lname").innerText = data.delivery_details.name.last_name;
+					document.getElementById("div_delivery_country").innerText = data.delivery_details.address.country;
+					document.getElementById("div_delivery_city").innerText = data.delivery_details.address.city;
+					document.getElementById("div_delivery_address").innerText = data.delivery_details.address.address;
+					document.getElementById("div_delivery_index").innerText = data.delivery_details.address.index;
+					document.getElementById("div_delivery_date").innerText = data.delivery_details.date;
+					
+					var change_delivery_lat = Number(data.delivery_details.address.latlng.lat);
+					var change_delivery_lng = Number(data.delivery_details.address.latlng.lng);
+					var change_latlng = L.latLng(change_delivery_lat, change_delivery_lng);
+					console.log(change_latlng);
+					var change_delivery_map = L.map("change_delivery_location").setView(change_latlng, 13);
+					L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+						maxZoom: 18,
+						id: 'mapbox/streets-v11',
+						tileSize: 512,
+						zoomOffset: -1,
+						accessToken: 'pk.eyJ1IjoibWFnZGFsZW5hMzE4IiwiYSI6ImNrZ2ljeHp3eTAyZXIydHN1bTFmcTdxZG0ifQ.UwB5VeAMvyI5EfZ6kA4KSQ'
+					}).addTo(change_delivery_map);
+					var change_delivery_marker = L.layerGroup().addTo(change_delivery_map);
+					var marker = L.marker(change_latlng).addTo(change_delivery_marker);
+					
+					//Displaying package info
+					document.getElementById("div_size").innerText = data.package_info.size;
+					document.getElementById("div_weight").innerText = data.package_info.weight;
+					
+					display_package_form.style.display = "grid";
 			})
 			.catch((error) => {
 			  console.error('Error:', error);
 			});
 			
-			document.getElementById("package_number_form").remove();
-			document.getElementById("display_package_form").style.visibility = "visible";
 		 }
 	}
 	
 	document.getElementById("send_button").onclick = function Send_package_display(){
-		document.getElementById("type_form").remove();
-		document.getElementById("package_number_form").remove();
-		document.getElementById("sender_details").style.visibility = "visible";
+		document.getElementById("type_form").style.display = "none";
+		document.getElementById("package_number_form").style.display = "none";
+		document.getElementById("sender_details").style.display = "grid";
 	}
 	
 	//Scrolling to the package formScrolling to the receiver form
@@ -55,7 +112,7 @@ window.onload = function load(){
 		if(pickup_fname && pickup_lname && pickup_country && pickup_city && pickup_address && pickup_index &&  pickup_date && pickup_fname.value!="" 
 		&&  pickup_lname.value!="" && pickup_city.value!="" && pickup_address.value!="" && pickup_index.value!="" && pickup_date.value!=""){
 			var new_section = document.getElementById("receiver_details");
-			new_section.style.visibility = "visible";
+			new_section.style.display = "grid";
 			new_section.scrollIntoView(true);
 		} else {
 			alert("Please fill in the required fields.")
@@ -78,11 +135,20 @@ window.onload = function load(){
 		if(delivery_fname && delivery_lname && delivery_country && delivery_city && delivery_address && delivery_index &&  delivery_date && delivery_fname.value!="" 
 		&&  delivery_lname.value!="" && delivery_city.value!="" && delivery_address.value!="" && delivery_index.value!="" && delivery_date.value!=""){
 			var new_section = document.getElementById("package_details");
-			new_section.style.visibility = "visible";
+			new_section.style.display = "grid";
 			new_section.scrollIntoView(true);
 		} else {
 			alert("Please fill in the required fields.")
 		}
+	}
+	
+	document.getElementById("lookup_homepage").onclick = function ReturnHome(){
+		document.getElementById("package_number_form").style.display = "none";
+		document.getElementById("sender_details").style.display = "none";
+		document.getElementById("receiver_details").style.display = "none";
+		document.getElementById("package_details").style.display = "none";
+		document.getElementById("display_package_form").style.display = "none";
+		document.getElementById("type_form").style.display = "grid";
 	}
 
 	//Map for Pickup
